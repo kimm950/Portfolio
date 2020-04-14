30450aa8b247bb0f1781392e682c638c97007a92
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-14 22:46:04 +0900

    getting started my project

---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Portfolio.css';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <h2>Portfolio</h2>
+    );
+  }
+}
+
+export default Portfolio;
