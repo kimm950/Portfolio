ffbbc8319f51db392454075b5080e36c0b3969f3
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-14 23:08:44 +0900

    import semantic ui path in the entry file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Portfolio from './Portfolio';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
