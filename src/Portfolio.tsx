4a7d966a807c579c4e8ece24ee0f6c3231c562ef
Kimm <hyunsung.kim@zeals.co.jp>
2020-06-07 14:50:43 +0900

    start working on layouts

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -12,6 +12,8 @@ import {
   Segment,
 } from 'semantic-ui-react';
 
+import Profile from './images/profile.png'
+
 class Portfolio extends Component {
   render() {
     return (
@@ -19,30 +21,22 @@ class Portfolio extends Component {
         <Menu fixed='top' inverted>
           <Container>
             <Menu.Item as='a' header>
-              <Image src='/images/profile.png' size='mini' style={{ marginRight: '1.5em' }} />
-            Portfolio
-        </Menu.Item>
+              <Image
+                src={`${Profile}`}
+                size='mini'
+                circular
+                style={{ marginRight: '1.5em' }}
+              />Portfolio</Menu.Item>
             <Menu.Item as='a'>Home</Menu.Item>
+            <Menu.Item as='a'>Works</Menu.Item>
+            <Menu.Item as='a'>About</Menu.Item>
 
-            <Dropdown item simple text='Dropdown'>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Header>Header Item</Dropdown.Header>
-                <Dropdown.Item>
-                  <i className='dropdown icon' />
-                  <span className='text'>Submenu</span>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
           </Container>
         </Menu>
+
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>HYUN SUNG KIM</Header>
+        </Container>
       </div>
     );
   }
