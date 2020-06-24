02b984ee83ea6c2e410bc6b647be316d78015a79
Kimm <hyunsung.kim@zeals.co.jp>
2020-06-24 13:38:39 +0900

    update hex

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -13,33 +13,43 @@ const Works = () => {
       <div className='menu-dir' style={{ backgroundColor: '#272727', color: 'white', padding: '7em', height: '100vh' }}>
         <List inverted>
           <List.Item >
-            <List.Icon name='folder' />
+            <List.Icon style={{
+              color: '#0088FE',
+            }} name='folder' />
             <List.Content>
               <List.Header>works</List.Header>
               <List.Description>projects</List.Description>
               <List.List>
                 <List.Item>
-                  <List.Icon name='folder' />
+                  <List.Icon style={{
+                    color: '#FFBB28',
+                  }} name='folder' />
                   <List.Content>
                     <List.Header>front_end</List.Header>
                     <List.Description>Front-end development</List.Description>
                   </List.Content>
                 </List.Item>
                 <List.Item>
-                  <List.Icon name='folder' />
+                  <List.Icon style={{
+                    color: '#FF8042',
+                  }} name='folder' />
                   <List.Content>
                     <List.Header>design</List.Header>
                     <List.Description>UI and UX // Graphic Design</List.Description>
                     <List.List>
                       <List.Item>
-                        <List.Icon name='folder' />
+                        <List.Icon style={{
+                          color: '#00C49F',
+                        }} name='folder' />
                         <List.Content>
                           <List.Header>ui_and_ux</List.Header>
                           <List.Description>User Interface and Experiences Design works</List.Description>
                         </List.Content>
                       </List.Item>
                       <List.Item>
-                        <List.Icon name='folder' />
+                        <List.Icon style={{
+                          color: '#0088FE',
+                        }} name='folder' />
                         <List.Content>
                           <List.Header>graphic_design</List.Header>
                           <List.Description>
@@ -54,13 +64,17 @@ const Works = () => {
             </List.Content>
           </List.Item>
           <List.Item>
-            <List.Icon name='folder' />
+            <List.Icon style={{
+              color: '#0088FE',
+            }} name='folder' />
             <List.Content>
               <List.Header>etc</List.Header>
               <List.Description>Other works</List.Description>
               <List.List>
                 <List.Item>
-                  <List.Icon name='folder' />
+                  <List.Icon style={{
+                    color: '#02a104',
+                  }} name='folder' />
                   <List.Content>
                     <List.Header>video_motion_graphic</List.Header>
                     <List.Description>
@@ -72,7 +86,9 @@ const Works = () => {
             </List.Content>
           </List.Item>
           <List.Item>
-            <List.Icon name='file' />
+            <List.Icon style={{
+              color: '#cc1818',
+            }} name='file' />
             <List.Content>
               <List.Header>resume.json</List.Header>
               <List.Description>My resume</List.Description>
